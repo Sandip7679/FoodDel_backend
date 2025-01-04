fe4fd185c0831964b1f312a3b024be68e1782d83
Sandip Bera <107534834+Sandip7679@users.noreply.github.com>
2025-01-04 23:41:15 +0530

    cloudinary

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 // import streamifier from 'streamifier';  // Import streamifier to convert buffer to readable stream
 import foodModel from "../models/foodModel.js";
 import fs from "fs";
+import uploadImageClodinary from "../utils/uploadImageClodinary.js";
 // import gfs from "../server.js";
 // import Grid from "gridfs-stream";
 
@@ -117,20 +118,25 @@ const listFood = async (req, res) => {
 //   }
 // };
 
+
+
 const addFood = async (req, res) => {
+
     try {
 
-        let image_filename = `${req.file.filename}`
+      const uploadedImage = await uploadImageClodinary(req.file);
+
+        // let image_filename = `${req.file.filename}`
         const food = new foodModel({
             name: req.body.name,
             description: req.body.description,
             price: req.body.price,
             category:req.body.category,
-            image: image_filename,
+            image: uploadedImage,
         })
 
         await food.save();
-        res.json({ success: true, message: "Food Added" })
+        res.json({ success: true, message: "Food Added",image:uploadedImage })
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: "Error" })
@@ -141,7 +147,7 @@ const addFood = async (req, res) => {
 const removeFood = async (req, res) => {
   try {
     const food = await foodModel.findById(req.body.id);
-    fs.unlink(`public/uploads/${food.image}`, () => {});
+    // fs.unlink(`public/uploads/${food.image}`, () => {});
 
     await foodModel.findByIdAndDelete(req.body.id);
     res.json({ success: true, message: "Food Removed" });
